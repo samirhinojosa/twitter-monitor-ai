dec0535115c8c8a8d1711a3daac7996c03082d8b
Samir Hinojosa <samirhinojosa@gmail.com>
2025-05-22 15:39:51 +0200

    Getting tweets of a specific user

--- app/infrastructure/services/twitter_service.py
+++ app/infrastructure/services/twitter_service.py
@@ -2,7 +2,7 @@ from typing import List
 import tweepy
 from app.interfaces.twitter_scraper import TwitterScraperInterface
 from app.core.settings import get_settings
-from app.domain.models.tweet import TweetModel
+from app.domain.schemas.tweet import TweetResponse
 
 # Runtime Settings/Environment Configuration
 settings = get_settings()
@@ -17,28 +17,44 @@ class TwitterScraperService(TwitterScraperInterface):
             access_token_secret=settings.X_ACCESS_TOKEN_SECRET
         )
 
-    def get_user_tweets(self, user_name: str, max_results: int = 5) -> List[TweetModel]:
+    def get_user_tweets(self, user_name: str, max_results: int = 5) -> List[TweetResponse]:
         
-        tweets: List[TweetModel] = []
+        tweets: List[TweetResponse] = []
 
         try:
             user_id = self.client.get_user(username=user_name).data.id
-            user_tweets = self.client.get_users_tweets(id=user_id, 
+            response = self.client.get_users_tweets(id=user_id, 
                                                     max_results=max_results, 
-                                                    tweet_fields=["created_at"])
-            for tweet in user_tweets:
-                print(f"Tweet: {tweet}")
-                # if "media" in tweet.entities:
-                if hasattr(tweet, "media"):
-                    media = tweet.entities.get("media", [])
-                    media_urls = [item["media_url_https"] for item in media] if media else None
+                                                    tweet_fields=["created_at", "attachments"],
+                                                    expansions=["attachments.media_keys"],
+                                                    media_fields=["media_key", "type", "url", "preview_image_url"],
+                                                    exclude=["retweets", "replies"]
+            )
 
-                tweets.append(TweetModel(
-                    id=tweet.id_str,
-                    user_name=tweet.user.screen_name,
-                    content=tweet.full_text,
-                    created_at=tweet.created_at,
-                    media_urls=media_urls
+            # getting the tweets
+            tweets_response = response.data or []
+            media_dict = {
+                media["media_key"]: media 
+                for media in response.includes.get("media", [])
+            } if response.includes and "media" in response.includes else {}
+             
+            for tweet in tweets_response:
+                # getting the medias in the tweet
+                media_keys = tweet.data.get("attachments", {}).get("media_keys", [])
+                media_urls = []
+                for key in media_keys:
+                    media = media_dict.get(key)
+                    if media:
+                        url = media.get("url") or media.get("preview_image_url")
+                        if url:
+                            media_urls.append(url)
+
+                tweets.append(TweetResponse(
+                    id = str(tweet.id),
+                    user_name = user_name,
+                    content = tweet.text,
+                    created_at = str(tweet.created_at),
+                    media_urls = media_urls
                 ))
 
         except tweepy.TooManyRequests as e:
